00e848de9db68633c85ca620442165d7199d803d
Никита Журавлёв <j_nikita@smartintel.ru>
2018-05-16 15:13:27 +0300

    take this

---
+++ Testpath/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Testpath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter streamWriter = new StreamWriter(@"testpath.txt");
+            streamWriter.WriteLine("Путь к файлу: " + Path.GetFullPath(@"testpath.txt"));
+            Console.WriteLine("Путь к файлу: " + Path.GetFullPath(@"testpath.txt") + "\n");
+
+            streamWriter.WriteLine("Рабочий путь: " + Environment.CurrentDirectory);
+            Console.WriteLine("Рабочий путь: " + Environment.CurrentDirectory + "\n");
+
+            Console.WriteLine("-Полная информация -" + "\n");
+
+            Console.WriteLine("GetEnvironmentVariables: ");
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+            {
+                streamWriter.WriteLine("  {0} = {1}", de.Key, de.Value);
+                Console.WriteLine("  {0} = {1}", de.Key, de.Value);
+            }
+            streamWriter.Close();
+            Console.ReadKey();
+                
+        }
+    }
+}
